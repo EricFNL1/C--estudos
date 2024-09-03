3ae6099368aa8a8845a352d03398e67ce86e7f13
EricF <eric.faria2003@gmail.com>
2024-09-03 12:03:34 -0300

    Feito Exercicio2

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program{
+
+    static void Main(string[] args){
+        Console.WriteLine("Digite o primeiro número");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine("Digite o segundo número");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+        int soma = num1 + num2;
+
+        Console.WriteLine($"A soma é, {soma} ");
+    }
+
+
+}
