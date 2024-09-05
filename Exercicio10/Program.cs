090e3e4e07048b3ac636cc12a4d80cb3858210c9
EricF <eric.faria2003@gmail.com>
2024-09-05 11:45:30 -0300

    Feito Exercicio 10, acabado Exercicios básicos

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class Program{
+
+    static void Main(string[] args){
+        Console.WriteLine("Insira a temepartura em C°: ");
+        int celsius = Convert.ToInt32(Console.ReadLine());
+
+        double conversao = (celsius * 1.8) + 32;
+
+        Console.WriteLine("" + conversao);
+    }
+
+}
