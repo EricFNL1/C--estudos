5fa03694a232a9a5c8311f0fa7657445da63d949
EricF <eric.faria2003@gmail.com>
2024-09-05 11:05:15 -0300

    Feito Exercicio9

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe um número para verificar se ele é primo: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num <= 1)
+        {
+            Console.WriteLine("O número " + num + " não é primo");
+        }
+        else if (EhPrimo(num))
+        {
+            Console.WriteLine("O número " + num + " é primo");
+        }
+        else
+        {
+            Console.WriteLine("O número " + num + " não é primo");
+        }
+    }
+
+    static bool EhPrimo(int num)
+    {
+        if (num <= 1) return false;
+        if (num == 2) return true; // 2 é o único número primo par
+
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num % i == 0)
+            {
+                return false; // Encontrou um divisor, então não é primo
+            }
+        }
+        return true; // Se não encontrou nenhum divisor, é primo
+    }
+}
---
+++ Exercicio9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program{
+
+    static void Main(string[] args){
+
+        int totalnotas = 0;
+       
+
+        Console.WriteLine("Insira o quantos notas serão inseridas: ");
+        int notas = Convert.ToInt32(Console.ReadLine());
+
+        for(int i = 0; i < notas; i++) {
+            Console.WriteLine("Insira a nota: ");
+            int dadosnota = Convert.ToInt32(Console.ReadLine());
+            totalnotas += dadosnota;
+        }
+         float calcfinal = totalnotas / notas;
+        Console.WriteLine("Média final: " +calcfinal);
+    }
+
+}
